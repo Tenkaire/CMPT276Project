bebbeb071854273989125b2e65b17e2748053d4c
cla280 <chang_liu_8@sfu.ca>
2017-02-28 10:50:25 -0800

    Replace QuizActivity.java

--- src/com/g8/group8/QuizActivity.java
+++ src/com/g8/group8/QuizActivity.java
@@ -2,17 +2,24 @@ package com.g8.group8;
 
 import android.app.Activity;
 import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 	
+	
 	TextView count, question, text1, text2, text3;
+	private QuestionLibrary questionL = new QuestionLibrary();
+	private int questionNum = 1;
+	private String CorrectAnswer; 
+	private Button previous, hint, skip, quit, save;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,10 @@ public class QuizActivity extends Activity {
 		text2.setOnLongClickListener(LongClickListener);
 		text3.setOnLongClickListener(LongClickListener);
 		question.setOnDragListener(DragListener);
+		
+		onClickButtonListenerForHint();
+		onClickButtonListenerForSkip();
+		onClickButtonListenerForQuit();
 		
 	}
 	
@@ -56,7 +67,7 @@ public class QuizActivity extends Activity {
 				
 				if(view.getId()== R.id.quiz_option){
 					view.setVisibility(View.INVISIBLE);
-					question.setText("Hello");
+					updateQuestions();
 					Toast.makeText(QuizActivity.this, "Dragged", Toast.LENGTH_LONG).show();
 				}
 				break;
@@ -65,7 +76,7 @@ public class QuizActivity extends Activity {
 			case DragEvent.ACTION_DROP:
 				final View view1 = (View) event.getLocalState();
 				if(view1.getId()== R.id.quiz_option){
-					question.setText("Hello hello");
+					updateQuestions();
 				}
 				break;
 			
@@ -74,5 +85,64 @@ public class QuizActivity extends Activity {
 		}
 		
 	};
+	
+	private void updateQuestions(){
+		question.setText(questionL.getQuestion(questionNum));
+		text1.setText(questionL.getOption1(questionNum));
+		text2.setText(questionL.getOption2(questionNum));
+		text3.setText(questionL.getOption3(questionNum));
+		
+		CorrectAnswer = questionL.getCorrect(questionNum);
+		if(questionNum<4){
+			questionNum++;
+		}
+		
+	}
+	
+	public void onClickButtonListenerForQuit(){
+        quit = (Button)findViewById(R.id.quiz_quit);
+        quit.setOnClickListener(
+             new View.OnClickListener() {
+
+             @Override
+             public void onClick(View v) {
+                  // TODO Auto-generated method stub
+                  Intent intent = new Intent(QuizActivity.this,MainActivity.class);
+                  startActivity(intent);
+             }
+        });
+   }
+	
+	public void onClickButtonListenerForSkip(){
+        skip = (Button)findViewById(R.id.quiz_skip);
+        skip.setOnClickListener(
+             new View.OnClickListener() {
+
+             @Override
+             public void onClick(View v) {
+                  // TODO Auto-generated method stub
+            	 
+            	 if(questionNum<4){
+            		 updateQuestions();
+            	 }else{
+            		 Toast.makeText(QuizActivity.this, "No more question!", Toast.LENGTH_LONG).show();
+            	 }
+             }
+        });
+   }
+	
+	public void onClickButtonListenerForHint(){
+        hint = (Button)findViewById(R.id.quiz_hint);
+        hint.setOnClickListener(
+             new View.OnClickListener() {
+
+             @Override
+             public void onClick(View v) {
+                  // TODO Auto-generated method stub
+            	 Toast.makeText(QuizActivity.this, "Correct Option is "+ questionL.getCorrect(questionNum-1), Toast.LENGTH_LONG).show();
+             }
+        });
+   }
+	
 
 }
