b98a1bac79c1e7de4eaa11bdc60a1c38e4f33894
cla280 <chang_liu_8@sfu.ca>
2017-03-05 03:50:39 -0800

    Replace HelpActivity.java

--- Eclipse Project/src/com/g8/group8/HelpActivity.java
+++ Eclipse Project/src/com/g8/group8/HelpActivity.java
@@ -1,34 +1,53 @@
 package com.g8.group8;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class HelpActivity extends Activity {
+	
+	Button Info_btn, Shop_btn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_help);
+		
+		onClickButtonListenerForInfo();
+		onClickButtonListenerForShop();
 	}
+	
+	 public void onClickButtonListenerForInfo(){
+	        Info_btn = (Button)findViewById(R.id.help_info);
+	        Info_btn.setOnClickListener(
+	             new View.OnClickListener() {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.help, menu);
-		return true;
-	}
+	             @Override
+	             public void onClick(View v) {
+	                  // TODO Auto-generated method stub
+	                  Intent intent = new Intent(HelpActivity.this,InformationActivity.class);
+	                  startActivity(intent);
+	             }
+	        });
+	   }
+	 
+	 public void onClickButtonListenerForShop(){
+	        Shop_btn = (Button)findViewById(R.id.help_shop);
+	        Shop_btn.setOnClickListener(
+	             new View.OnClickListener() {
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	             @Override
+	             public void onClick(View v) {
+	                  // TODO Auto-generated method stub
+	                  Intent intent = new Intent(HelpActivity.this,ShopActivity.class);
+	                  startActivity(intent);
+	             }
+	        });
+	   }
+
+
 }
