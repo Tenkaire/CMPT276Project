0ae025d9a5088faed84c394e7f56a08d11725afb
tkasper <tkasper@sfu.ca>
2017-03-18 12:25:16 -0700

    Restarted Creating Question Selection Class

---
+++ DragAndCode/app/src/main/java/com/group8/dragcode/QuestionButtonAdapter.java
@@ -0,0 +1,10 @@
+package com.group8.dragcode;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Tyler on 2017-03-18.
+ */
+
+public class QuestionButtonAdapter extends BaseAdapter {
+}
--- DragAndCode/app/src/main/java/com/group8/dragcode/QuestionSelectionActivity.java
+++ DragAndCode/app/src/main/java/com/group8/dragcode/QuestionSelectionActivity.java
@@ -1,10 +1,7 @@
 package com.group8.dragcode;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.GridView;
 
 public class QuestionSelectionActivity extends AppCompatActivity {
 
@@ -12,13 +9,5 @@ public class QuestionSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_selection_layout);
-
-        GridView gridView = (GridView) findViewById(R.id.buttonGrid);
-        gridView.setAdapter(new QuestionNumberAdapter(this));
-    }
-
-    public void returnToMainMenu(View view){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 }
