e401e8fb07e44c0d0046eaabb39631ba109fb354
tkasper <tkasper@sfu.ca>
2017-03-17 23:27:11 -0700

    Basic Layout Of Question Selection Complete

---
+++ DragAndCode/app/src/main/java/com/group8/dragcode/QuestionNumberAdapter.java
@@ -0,0 +1,17 @@
+package com.group8.dragcode;
+
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+
+/**
+ * Created by Tyler on 2017-03-17.
+ */
+public class QuestionNumberAdapter extends BaseAdapter {
+
+    public int getCount(){
+
+    }
+
+    public QuestionNumberAdapter() {
+    }
+}
