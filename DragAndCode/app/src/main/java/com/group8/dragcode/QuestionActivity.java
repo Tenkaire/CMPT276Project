a2c0dd4e000ea1d0c2a33458de6805bbdadd4787
Liam Wilson <lpw3@sfu.ca>
2017-03-18 18:28:39 -0700

    Readded QuestionActivity

---
+++ DragAndCode/app/src/main/java/com/group8/dragcode/QuestionActivity.java
@@ -0,0 +1,46 @@
+package com.group8.dragcode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+import com.group8.dragcode.qclasses.Module;
+import com.group8.dragcode.qclasses.Question;
+import com.group8.dragcode.qclasses.XMLReader;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    private Question question;
+    private String qKey;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+
+        Intent intent = getIntent();
+        this.qKey = intent.getStringExtra("QKEY");
+
+        XMLReader xml = new XMLReader(this);
+        try {
+            question = xml.getQuestion(qKey);
+        } catch (IOException | XmlPullParserException e)
+        {
+            Log.e("QuestionActivity", "Failed to get question " + qKey + ". Error: " + e.getMessage());
+        }
+
+        LinearLayout ll_modules = (LinearLayout) findViewById(R.id.ll_modules);
+
+        for (Module m : question.getCodeModules())
+        {
+            ll_modules.addView(m);
+        }
+
+    }
+}
