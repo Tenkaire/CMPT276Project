9a6ad2e08598109fde7246d50a89c6192e6d434e
Liam Wilson <lpw3@sfu.ca>
2017-03-19 17:29:46 -0700

    Answer Checking added to QuestionActivity

--- DragAndCode/app/src/main/java/com/group8/dragcode/QuestionActivity.java
+++ DragAndCode/app/src/main/java/com/group8/dragcode/QuestionActivity.java
@@ -1,12 +1,16 @@
 package com.group8.dragcode;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import com.group8.dragcode.qclasses.Answer;
+import com.group8.dragcode.qclasses.Blank;
 import com.group8.dragcode.qclasses.Question;
 import com.group8.dragcode.qclasses.XMLReader;
 
@@ -49,5 +53,75 @@ public class QuestionActivity extends AppCompatActivity
         {
             ll_answers.addView(answer);
         }
+
+        this.setTitle(question.getQuestionTitle());
+    }
+
+    public void onClickCheckAnswer(View view)
+    {
+        boolean answersCorrect = true;
+
+        LinearLayout ll_code = (LinearLayout) findViewById(R.id.ll_code);
+
+        for (int i = 0; i < ll_code.getChildCount(); i++)
+        {
+            View row = ll_code.getChildAt(i);
+
+            if (row instanceof LinearLayout)
+            {
+                LinearLayout ll_row = (LinearLayout) row;
+                for (int j = 0; j < ll_row.getChildCount(); j++)
+                {
+                    View rowChild = ll_row.getChildAt(j);
+
+                    if (rowChild instanceof Blank)
+                    {
+                        Blank blank = (Blank) rowChild;
+
+                        if (!blank.checkAnswer())
+                        {
+                            answersCorrect = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(QuestionActivity.this);
+        String title;
+        String text;
+
+        if (answersCorrect)
+        {
+            title = getResources().getString(R.string.alert_correct);
+            text = getResources().getString(R.string.alert_correct_text);
+            builder.setTitle(title).setMessage(text).setNeutralButton(getResources().getString(android.R.string.ok), new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    dialog.dismiss();
+                    // Set the question as finished in the database here
+
+                    finish();
+                }
+            });
+        }
+        else
+        {
+            title = getResources().getString(R.string.alert_incorrect);
+            text = getResources().getString(R.string.alert_incorrect_text);
+            builder.setTitle(title).setMessage(text).setNeutralButton(getResources().getString(android.R.string.ok), new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    dialog.dismiss();
+                }
+            });
+        }
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
